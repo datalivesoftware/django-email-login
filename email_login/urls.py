614363f7f058f151aa1baa089ad2b05c88ba3ec7
Tino de Bruijn <tinodb@gmail.com>
2015-10-30 22:10:54 +0700

    Update to new django 1.7+ url patterns syntax

--- email_login/urls.py
+++ email_login/urls.py
@@ -1,12 +1,12 @@
 # We need to override login view, the rest is ok.
-from django.conf.urls import *
+from django.conf.urls import url
 from django.contrib.auth.urls import urlpatterns as auth_urls
 from forms import EmailAuthenticationForm
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^login/$', 'django.contrib.auth.views.login',
-        { 'template_name' : 'email_login/login.html',
-          'authentication_form' : EmailAuthenticationForm}, name='auth_login'),
-)
+        {'template_name': 'email_login/login.html',
+         'authentication_form': EmailAuthenticationForm}, name='auth_login'),
+]
 
 urlpatterns += auth_urls
