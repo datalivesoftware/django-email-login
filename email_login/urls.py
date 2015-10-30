2cb1b32de422b80ca5f5e309fa375053e6b026ce
Tino de Bruijn <tinodb@gmail.com>
2015-10-30 22:13:08 +0700

    Also don't use string view arguments for url anymore

--- email_login/urls.py
+++ email_login/urls.py
@@ -1,10 +1,12 @@
 # We need to override login view, the rest is ok.
+import django.contrib.auth.views.login as auth_login_view
 from django.conf.urls import url
 from django.contrib.auth.urls import urlpatterns as auth_urls
-from forms import EmailAuthenticationForm
+
+from .forms import EmailAuthenticationForm
 
 urlpatterns = [
-    url(r'^login/$', 'django.contrib.auth.views.login',
+    url(r'^login/$', auth_login_view,
         {'template_name': 'email_login/login.html',
          'authentication_form': EmailAuthenticationForm}, name='auth_login'),
 ]
